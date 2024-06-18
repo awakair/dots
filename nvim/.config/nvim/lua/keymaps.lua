90390d391f65a4c439df68b851b2e32e99f28fb8
Dmitry Sukhov <awakair@yandex.ru>
2024-06-18 13:19:37 +0300

    update neovim config

---
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -0,0 +1,32 @@
+-- define common options
+local opts = {
+    noremap = true,      -- non-recursive
+    silent = true,       -- do not show message
+}
+
+-----------------
+-- Normal mode --
+-----------------
+
+-- Hint: see `:h vim.map.set()`
+-- Use <C-h/j/k/l> to move the cursor among windows
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+
+-- Use Ctrl + arrow keys to resize windows
+-- delta: 2 lines
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+
+-----------------
+-- Visual mode --
+-----------------
+
+-- Hint: start visual mode with the same area as the previous area and the same mode
+vim.keymap.set('v', '<', '<gv', opts)
+vim.keymap.set('v', '>', '>gv', opts)
+
---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,39 @@
+-- Hint: use `:h <option>` to figure out the meaning if needed
+vim.opt.clipboard = 'unnamedplus'   -- use system clipboard 
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
+
+-- Tab
+vim.opt.tabstop = 4                 -- number of visual spaces per TAB
+vim.opt.softtabstop = 4             -- number of spacesin tab when editing
+vim.opt.shiftwidth = 4              -- insert 4 spaces on a tab
+vim.opt.expandtab = true            -- tabs are spaces, mainly because of python
+vim.optautoindent = true            -- indent a new line the same amount as the line just typed
+
+-- UI config
+vim.opt.number = true               -- show absolute number
+vim.opt.relativenumber = true       -- add numbers to each line on the left side
+vim.opt.cursorline = true           -- highlight cursor line underneath the cursor horizontally
+vim.opt.splitbelow = true           -- open new vertical split bottom
+vim.opt.splitright = true           -- open new horizontal splits right
+-- vim.opt.showmatch = true            -- show matching brackets
+vim.opt.cc = '80,120'               -- column border at 80 and 120 symbols
+vim.opt.syntax = on                 -- syntax highlighting
+vim.opt.spell = on                  -- enable spell check TODO: download language packages
+-- vim.opt.termguicolors = true        -- enable 24-bit RGB color in the TUI
+-- vim.opt.showmode = false            -- don't need the "-- INSERT --" mode hint
+
+-- Searching
+vim.opt.incsearch = true            -- search as characters are entered
+vim.opt.hlsearch = true             -- highlight matches
+vim.opt.showmatch = true            -- show match TODO: diff from hlsearch?
+vim.opt.incsearch = true            -- TODO: ???
+vim.opt.ignorecase = true           -- ignore case in searches by default
+vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
+
+-- Swap/Backup
+vim.opt.swapfile = false            -- disable creating swap file
+vim.opt.backupdir = '~/.cache/vim'  -- directory to store backup files
+
+-- set wildmode=longest,list   " get bash-like tab completions
+
